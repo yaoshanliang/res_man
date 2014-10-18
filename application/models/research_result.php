0d6b1a537caeab0d6ac53a23a922b8a9f5be4dd3
Carpela <carpela@163.com>
2014-10-18 08:54:29 +0800

    basic model

---
+++ application/models/research_result.php
@@ -0,0 +1,30 @@
+<?php
+	class Research_result extends CI_Model
+	{
+		public function search_name($name)
+		{
+			
+		}
+		public function search_person()
+		{
+
+		}
+		public function search_category()
+		{
+
+		}
+		public function search_year()
+		{
+
+		}
+		public function insert()
+		{
+
+		}
+		public function update()
+		{
+
+		}
+
+	}
+?>
