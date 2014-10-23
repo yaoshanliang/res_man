9a7bc9ec19a29248b71ce641d4aacfbebec65b56
carpela <carpela@163.com>
2014-10-23 19:28:33 +0800

    与科研项目有关的数据

--- table.sql
+++ table.sql
@@ -1,6 +1,28 @@
-create table awards(id int primary key not null, category text, level text, time Date, Persons text);
+##与项目有关(FK 在非innodb不可用)
 
-create table verfication(id int primary key not null, time Date, organization text, others text);
+#人员 person
+create table person(id int primary key not null auto_increment,name varchar(10) not null,duties text);
 
-create table project(Name text,Source int,Admin int,Start text,End text,Money float,MoneyType char(1),
-	Contract text,Credit text,,MoneyManage int,Verfication int,Awards int);
+#项目来源 source
+create table source(name text not null);
+
+#科研项目 project
+create table project(`projectid` int primary key not null auto_increment,name text not null,`source` text not null,`level` text
+	,`principal` int not null,`start` text not null,`end` text not null,`money` float not null
+	,`currency` varchar(10) not null default "RMB",`contract` text not null,credit text not null,`type` text not null,
+	foreign key(principal) references person(id));
+
+#获奖情况 award
+create table award(projectid int not null,id int not null,`order` int not null, time text not null, 
+	foreign key(id) REFERENCES person(id));
+
+#鉴定验收 validation
+create table validation(projectid int not null,time text not null,institute text not null,others text,
+	foreign key(projectid) references project(projectid));
+
+#经费到款 funds
+create table funds(projectid int not null,payoff float not null,remain float not null,others text);
+
+#年份到款表 detail
+create table detail(projectid int not null,year int not null,currency varchar(10) not null default "RMB",figure float not null);
+	
