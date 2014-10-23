7031e786b5bdafaadfae8be210ebb748332c67b6
carpela <carpela@163.com>
2014-10-23 14:14:06 +0800

    完善修改用户名密码

--- application/models/admin.php
+++ application/models/admin.php
@@ -1,30 +1,30 @@
 <?php
 	class Admin extends CI_Model
 	{
+		//获取用户名
 		public function getUser()
 		{
 			$res = $this->db->get("Admin");
 			$obj = $res->result();
 			return $obj[0]->Name;
 		}
-
+		//获取密码
 		public function getPasswd()
 		{
 			$res = $this->db->get("Admin");
 			$obj = $res->result();
 			return $obj[0]->Password;
 		}
-
+		// 更改用户名
 		public function setUser($user)
 		{
-
-			$res = $this->db->update("update Admin set Name=".$user);
+			$res = $this->db->update('Admin',array('Name'=>$user));
 			return $res;
 		}
-
+		// 更改密码
 		public function setPasswd($passwd)
 		{
-			$res = $this->db->update("update Admin set Password=".$passwd);
+			$res = $this->db->update('Admin',array('Password'=>md5($passwd)));
 			return $res;
 		}
 	}
