49043f39b3445668afbe90b32909b9cd4b17f4ce
carpela <carpela@163.com>
2014-11-13 23:04:26 +0800

    新的界面

--- application/views/patent/list.php
+++ application/views/patent/list.php
@@ -4,52 +4,61 @@
       $("table tr:gt(0)").click(function()
       {
         var index = $(event.target).index(); //列索引
-        if(index == 7) return;
-        if(index == 6)
+        if(index == 7 || index==8) return;
+        var current_mode = $("#currentMode").text();
+        if(current_mode == 0)
         {
-          //修改人员名单
-          $("#addListBtn").click();
-          // alert("you failed!");
           return;
-        }
-        if(index==0)
+        }else if(current_mode == 2)
         {
           if($(event.target).text() == $(event.target).parent().children().last().html()) return;
-          var data = { number: $(event.target).text() };
+          var data = { number: $(event.target).parent().children().first().html() };
           $.post("<?=site_url('patentmanage/delete')?>",data,function(res,status)
           {
             $("#refresh_list").click();
             alert(res);
           });
           return;
+        }else if(current_mode == 1)
+        {
+          if(index == 6) 
+          {
+            // 处理人员名单
+            window.location="<?=site_url('patentmanage/fileandlist')?>?number="+$(event.target).parent().children().first().html();
+            return;
+          }
+          if(index == 7) 
+          {
+            return;
+          }
+          var colname = $(event.target).parent().parent().children().first().children().eq(index).html();//列名
+          var value = $(event.target).text();
+          var result = prompt("请输入新的"+colname+":",value);
+          if(result == "" || result== null) //取消 输入为空 则不修改
+            return;
+          var opid = $(event.target).parent().children().first().html();
+          if(index == 1)
+          {
+            var data={ number: opid, name: result, which: 'name' };
+          }else if(index == 2)
+          {
+            var data={ number: opid, register: result, which: 'register' };
+          }else if(index == 3)
+          {
+            var data={ number: opid, person: result, which: 'person' };
+          }else if(index == 4)
+          {
+            var data={ number: opid, institute: result, which: 'institute' };
+          }else if(index == 5)
+          {
+            var data={ number: opid, time: result, which: 'time' };
+          }
+          $.post("<?=site_url('patentmanage/modify')?>",data,function(res,status)
+          {
+            $("#refresh_list").click();
+            alert(res);
+          });
         }
-        var colname = $(event.target).parent().parent().children().first().children().eq(index).html();//列名
-        var value = $(event.target).text();
-        var result = prompt("请输入新的"+colname+":",value);
-        if(result == "" || result== null) //取消 输入为空 则不修改
-          return;
-        var opid = $(event.target).parent().children().first().html();
-        if(index == 1)
-        {
-          var data={ number: opid, name: result, which: 'name' };
-        }else if(index == 2)
-        {
-          var data={ number: opid, register: result, which: 'register' };
-        }else if(index == 3)
-        {
-          var data={ number: opid, person: result, which: 'person' };
-        }else if(index == 4)
-        {
-          var data={ number: opid, institute: result, which: 'institute' };
-        }else if(index == 5)
-        {
-          var data={ number: opid, time: result, which: 'time' };
-        }
-        $.post("<?=site_url('patentmanage/modify')?>",data,function(res,status)
-        {
-          $("#refresh_list").click();
-          alert(res);
-        });
       });
     });
     </script>
@@ -57,7 +66,7 @@
 
 <table class="table table-striped table-hover">
    <tr>
-    <td>编号</td>
+    <td hidden>编号</td>
     <td>名称</td>
     <td>专利权号</td>
     <td>专利权人</td>
@@ -68,27 +77,24 @@
   </tr>
 <?php foreach($patent as $item): ?>
   <tr>
-    <td><?=$item->number?></td>
+    <td hidden><?=$item->number?></td>
     <td><?=$item->name?></td>
     <td><?=$item->register?></td>
-    <td><?=$item->person?></td>
+    <td><?php
+      $res = $this->db->where('id',$item->person)->get('person');
+      echo $res->row()->name;
+    ?></td>
     <td><?=$item->institute?></td>
     <td><?=$item->time?></td>
     <td>
     <?php 
     // 获取人员名单 restrinct: <9
-    $res = $this->db->where('identifier',$item->number)->get('patentlist')->result();
+    $res = $this->db->where('identifier',$item->number)->order_by('order')->get('patentlist')->result();
     $str = "";
     foreach($res as $item2)
     {
-      for($i=0;$i<10;$i++)
-      {
-        if($i == $item2->order)
-        {
-          $res = $this->db->where('id',$item2->id)->get('person');
-          $str .= $res->row()->name.",";
-        }
-      }
+        $res = $this->db->where('id',$item2->id)->get('person');
+        $str .= $res->row()->name.",";
     }
     echo rtrim($str,',');
     ?>
@@ -99,3 +105,6 @@
   </tr>
 <?php endforeach; ?>
 </table>
+<br>
+<div id="fileandlist">
+</div>
--- application/views/patent/main.php
+++ application/views/patent/main.php
@@ -29,44 +29,24 @@
         $("#modeControl").submit();
       });
 
-      $("#remove_record").click(function()
+      $("#addRecord").click(function()
       {
-        $("#upload").hide();
-        $("#remove").slideToggle();
-      });
-
-      $("#upload_file").click(function()
-      {
-        $("#remove").hide();
-        $("#upload").slideToggle();
-      });
-
-      $(":submit").click(function()
-      {
-        if($(event.target).text() == "删除")
+        if($("#currentMode").text()!=2)
         {
-          $("#remove").hide();
-          var data = {
-            number: $("#reinputNumber").val()
-          };
-          $.post("<?=site_url('patentmanage/delete')?>",data,function(res,status)
-            {
-              alert(res);
-            }); 
-        }else if($(event.target).text() == "添加")
-        {
-          var data = {
-            name: $("#inputName").val(),
-            register: $("#inputRegister").val(),
-            person: $("#inputPerson").val(),
-            institute: $("#inputInstitute").val(),
-            time: $("#inputTime").val()
-          };
-          $.post("<?=site_url('patentmanage/add')?>",data,function(res,status)
-            {
-              alert(res);
-            });
+          alert("权限错误");
+          return;
         }
+        var data = {
+          name: $("#inputName").val(),
+          register: $("#inputRegister").val(),
+          person: $("#inputPerson").val(),
+          institute: $("#inputInstitute").val(),
+          time: $("#inputTime").val()
+        };
+        $.post("<?=site_url('patentmanage/add')?>",data,function(res,status)
+          {
+            alert(res);
+          });
         // 刷新一次数据
         $("#refresh_list").click(); 
         return true;
@@ -96,18 +76,6 @@
     <div>
         <a class="btn btn-default" id="refresh_list">刷新列表</a>
         <a class="btn btn-default" data-toggle="modal" data-target="#addModal">添加信息</a>
-        <a class="btn btn-default" id="remove_record">删除记录</a>
-        <a class="btn btn-default" id="upload_file">上传证明文件</a>
-        <a class="btn btn-default" hidden id="addListBtn" data-toggle="modal" data-target="#addList" href="<?=site_url('patentmanage/patentlist')?>">
-          添加人员名单
-        </a>
-
-          <div class="modal fade" id="addList" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-              <div class="modal-content">
-              </div>
-            </div>
-          </div>
           
          <div class="modal fade" id="addModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
             <div class="modal-dialog">
@@ -150,7 +118,7 @@
                           </div>
                           <div class="form-group">
                             <div class="col-sm-offset-3 col-sm-2">
-                              <button type="submit" class="btn btn-default">添加</button>
+                              <button type="submit" id="addRecord" class="btn btn-default">添加</button>
                             </div>
                           </div>
                       </form>
@@ -160,31 +128,7 @@
           </div>
     </div>
     <br/>
-    <div id="remove" hidden>
-         <form class="form-inline">
-          <div class="form-group">
-            <label class="sr-only" for="reinputNumber">编号</label>
-            <input type="text" name="number" class="form-control" id="reinputNumber" placeholder="编号">
-          </div>
-          <button type="submit" class="btn btn-default">删除</button>
-        </form>
-    </div>
     
-    <div class="row" id="upload" hidden>
-    <form role="form" class="form-inline" action="<?=site_url('patentfile/file_upload')?>" method="post" enctype="multipart/form-data">
-      <div class="form-group col-sm-2">
-        <label for="inputFileNumber" class="sr-only">编号</label>
-        <input type="text" class="form-control" id="inputFileNumber" placeholder="编号">
-      </div>
-      <div class="form-group col-sm-2">
-        <label for="inputFile" class="sr-only">证明文件</label>
-        <input type="file" id="inputFile" name="file">
-        <p class="help-block">只支持jpg,png,gif文件</p>
-      </div>
-      <button type="submit" class="btn btn-default">上传</button>
-    </form>
-    </div>
-    <br/>
     <div id="detail">
     </div> 
 
