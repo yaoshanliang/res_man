a55f51021e8f8f8db664ce80f89ad2b6d63c765f
carpela <carpela@163.com>
2014-11-17 17:54:07 +0800

    完成计算工作量（科研经费分值),其他的很难计算

--- application/controllers/search.php
+++ application/controllers/search.php
@@ -181,5 +181,78 @@
 
 			$this->load->view('search/year',$data);
 		}
+
+		public function work()
+		{
+			$this->load->view('search/workcount');
+		}
+
+		public function aboutWork()
+		{
+			$this->load->model('person');
+			$this->load->model('source');
+			$this->load->model('projectlist');
+			$this->load->model('project');
+
+			$year = $this->input->post('year');
+			$personname = $this->input->post("person");
+			$person = $this->person->getPersonByName($personname);
+			if($person == null)
+			{
+				echo "人员不存在";
+				return ;
+			}
+			$id = $person->id;
+			// 获得参与项目
+			$project_score = 0.0;
+			$res = $this->projectlist->getProjectID($id);
+			$project = array();
+			foreach($res as $item)
+			{
+				$project[] = ($this->project->getProjectByID($item->projectid));
+			}
+			foreach($project as $item)
+			{
+				if(intval($item->start) <= $year && intval($item->end) >= $year)
+				{
+					if($item->source == 1) 
+					{
+						$project_score += $item->money/(intval($item->end)+1-intval($item->start))*12.0;
+					}else if($item->source == 2)
+					{
+						$project_score += $item->money/(intval($item->end)+1-intval($item->start))*10.0;
+					}else if($item->source == 3)
+					{
+						if($item->type == "重大/重点")
+							$project_score += $item->money/(intval($item->end)+1-intval($item->start))*8.0;
+						else 
+							$project_score += $item->money/(intval($item->end)+1-intval($item->start))*5.0;
+					}else if($item->source == 4)
+					{
+						if($item->type == "重大/重点")
+							$project_score += $item->money/(intval($item->end)+1-intval($item->start))*4.0;
+						else 
+							$project_score += $item->money/(intval($item->end)+1-intval($item->start))*2.0;
+					}else if($item->source == 5)
+					{
+						$project_score += $item->money/(intval($item->end)+1-intval($item->start))*1.8;
+					}else if($item->source == 6)
+					{
+						$project_score += $item->money/(intval($item->end)+1-intval($item->start))*1.3;
+					}else if($item->source == 7)
+					{
+						$project_score += $item->money/(intval($item->end)+1-intval($item->start))*1.5;
+					}else
+					{
+						$project_score += $item->money/(intval($item->end)+1-intval($item->start))*1.0;
+					}
+				}
+			}
+			$data['personname'] = $personname;
+			$data['year']  = $year;
+			$data['project_score'] = number_format($project_score, 2, '.', '');
+			$this->load->view('search/work',$data);
+
+		}
 	}
 ?>
